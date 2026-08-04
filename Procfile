7643b45bb5ed66b2ae9395ded53a327aac68b70c
felociouke <felociouske@gmail.com>
2026-08-05 01:15:39 +0300

    Updated the Profileto collectstatic files

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn mulaearn_backend.wsgi --log-file -
+release: python manage.py migrate --noinput && python manage.py collectstatic --noinput
--- mulaearn_backend/settings.py
+++ mulaearn_backend/settings.py
@@ -88,14 +88,23 @@ CHANNEL_LAYERS = {
     },
 }
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+DATABASE_URL = config("DATABASE_URL", default=None)
 
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 STORAGES = {
     "default": {
